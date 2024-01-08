74b2ca16d9f14080d03d289d98e242e220d446f3
sqmasep <leblond.romain@orange.fr>
2024-01-08 10:35:40 +0100

    [INIT] Initialized Qwik project with tailwind

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <>
+      <div class="container-flex container">
+        <div>hey</div>
+      </div>
+    </>
+  );
+});
